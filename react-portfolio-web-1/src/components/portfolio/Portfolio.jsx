030defc6de4724bd363495b2ed38ef0b78449178
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-12-10 12:31:10 +0500

    Portfolio Section Completed
    The design and implementation of the portfolio section is completed. We have also inserted the data into the cards using the array model in REACTJS library.

--- react-portfolio-web-1/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-web-1/src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,46 @@ import IMG3 from "../../assets/p3.png"
 import IMG4 from "../../assets/p4.png"
 import IMG5 from "../../assets/p5.png"
 
+
+// Inserting Data through this array into the cards
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Spotify Clone',
+    github: 'https://github.com/AbdullahButt2611/Projects/tree/main/Web%20Applications/Spotify%20Clone',
+    demo: '#'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Jazz Cash App',
+    github: 'https://github.com/AbdullahButt2611/Jazz-App',
+    demo: 'https://github.com/AbdullahButt2611/Jazz-App/blob/main/README.md'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'AIC Website',
+    github: 'https://github.com/AbdullahButt2611/AIC-Website',
+    demo: '#'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Stance DJ - Blogging Site',
+    github: 'https://github.com/AbdullahButt2611/StanceDJ',
+    demo: 'http://stancedj.com/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'MAx Designs - Web Application',
+    github: 'https://github.com/AbdullahButt2611/MaxDesigns',
+    demo: 'https://max-designs.herokuapp.com/'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -13,61 +53,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/AbdullahButt2611?tab=repositories" className='btn' target="_blank">Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/AbdullahButt2611?tab=repositories" className='btn' target="_blank">Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/AbdullahButt2611?tab=repositories" className='btn' target="_blank">Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/AbdullahButt2611?tab=repositories" className='btn' target="_blank">Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/AbdullahButt2611?tab=repositories" className='btn' target="_blank">Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/AbdullahButt2611?tab=repositories" className='btn' target="_blank">Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-        </article>
-        
+        {
+          data.map(({id, image, title, github, demo}) => {
+              return(
+                <article key={id} className='portfolio__item'>
+                  <div className="portfolio__item-image">
+                    <img src={image} alt={title} />
+                  </div>
+                  <h3>{title}</h3>
+                  <div className="cta">
+                    <a href={github} className='btn' target="_blank">Github</a>
+                    <a href={demo} className='btn btn-primary' target="_blank">Live Demo</a>
+                  </div>
+                </article>
+              )
+          })
+        }
       </div>
     </section>
   )
