021a0601728ae6b252400934c3f6b0bb1d7a4bf1
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-12-12 22:30:03 +0500

    Working on Contact
    The contact section is in progress now.

--- react-portfolio-web-1/src/components/contact/Contact.jsx
+++ react-portfolio-web-1/src/components/contact/Contact.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {SiWhatsapp} from 'react-icons/si'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>abutt2210@gmail.com</h5>
+            <a href="mailto:abutt2210@gmail.com" target="_blank">Send an Email</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine/>
+            <h4>Messenger</h4>
+            <h5>Abdullah Butt</h5>
+            <a href="https://m.me/people/Abdullah-Butt/100052355030712/" target="_blank">Send a Message</a>
+          </article>
+
+          <article className="contact__option">
+            <SiWhatsapp/>
+            <h4>WhatsApp</h4>
+            <h5>03170047971</h5>
+            <a href="https://api.whatsapp.com/send?phone=03170047971" target="_blank">Let's Chat</a>
+          </article>
+
+        </div>
+        {/* End of Options */}
+
+        <form action="">
+          <input type="text" name='name' placeholder='Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 export default Contact
