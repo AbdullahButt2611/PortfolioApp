15b5a9df66531ffcf0b554f07be0a76e1c4a66ac
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-12-13 19:51:59 +0500

    Contact's Design work is completed
    The designing of the web distrubution of contact section is complete. Now we are working on the EmailJS to send the email to our email through the form we have created.

--- react-portfolio-web-1/src/components/contact/Contact.jsx
+++ react-portfolio-web-1/src/components/contact/Contact.jsx
@@ -14,21 +14,21 @@ const Contact = () => {
         <div className="contact__options">
 
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>abutt2210@gmail.com</h5>
             <a href="mailto:abutt2210@gmail.com" target="_blank">Send an Email</a>
           </article>
 
           <article className="contact__option">
-            <RiMessengerLine/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Abdullah Butt</h5>
             <a href="https://m.me/people/Abdullah-Butt/100052355030712/" target="_blank">Send a Message</a>
           </article>
 
           <article className="contact__option">
-            <SiWhatsapp/>
+            <SiWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>03170047971</h5>
             <a href="https://api.whatsapp.com/send?phone=03170047971" target="_blank">Let's Chat</a>
